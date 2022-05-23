481081fff193bdeb5de58a5b26098b94881b7df5
Uwish <pin-999@yandex.ru>
2022-05-23 19:38:42 +0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+let courses = [
+    { name: "Courses in England", prices: [0, 100] },
+    { name: "Courses in Germany", prices: [500, null] },
+    { name: "Courses in Italy", prices: [100, 200] },
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 200];
+let requiredRange3 = [200, null];
+
+const filterCourses = (courses, priceRange) => {
+    return courses.filter((course) => {
+        if (!priceRange[0]) {
+            return course.prices[0] <= priceRange[1] && (course.prices[1] <= priceRange[1] || !course.prices[1])
+        }
+        if (!priceRange[1]) {
+            return (course.prices[1] >= priceRange[0]) || !course.prices[1]
+        }
+        return course.prices[0] <= priceRange[0] && (course.prices[1] <= priceRange[1] || !course.prices[1])
+    })
+}
+// function filterFunc(courses, range) {
+//
+//     let searchedCourses = []
+//
+//     for (let i=0; i < courses.length; i++) {
+//         if (courses[i].prices[1] <= range[1] && range[1] >= courses[i].prices[0] && range[0] === null) {
+//             searchedCourses.push(courses[i])
+//         } else if (courses[i].prices[0] >= range[0] && range[1] <= courses[i].prices[0] && range[1] === null) {
+//             searchedCourses.push(courses[i])
+//         } else if (courses[i].prices[0] >= range[0] && courses[i].prices[1] <= range[1] && range[1] >= courses[i].prices[0]) {
+//             searchedCourses.push(courses[i])
+//         }
+//     }
+//     console.log(searchedCourses)
+// }
+
+filterCourses([...courses],[...requiredRange2])
